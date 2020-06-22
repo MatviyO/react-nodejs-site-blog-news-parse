8d2136df86645859ff50888d4f28b00990ff13c4
MatviyO <33325458+MatviyO@users.noreply.github.com>
2020-06-22 19:46:58 +0300

    se

---
+++ src/client/src/components/Post.js
@@ -0,0 +1,17 @@
+import React  from 'react';
+
+const Post = props => {
+    return (
+        <div className="post">
+            <div className="post_image"></div>
+            <div className="post_info">
+                <h2 className="post_title">title</h2>
+                <p className="post_description" >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, provident!</p>
+            </div>
+
+        </div>
+    )
+}
+
+
+export default Post;
